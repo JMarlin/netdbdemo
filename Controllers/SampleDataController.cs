f10d7712a4db54050208b4a20a650b41621061ee
Joseph Marlin <j.marlin@outlook.com>
2018-02-16 22:25:13 -0500

    Moved weather forecast to an external model class

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -9,36 +9,18 @@ namespace netdbdemo.Controllers
     [Route("api/[controller]")]
     public class SampleDataController : Controller
     {
-        private static string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
 
         [HttpGet("[action]")]
-        public IEnumerable<WeatherForecast> WeatherForecasts()
+        public IEnumerable<Models.WeatherForecast> WeatherForecasts()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+
+            return Enumerable.Range(1, 5).Select(index =>
             {
-                DateFormatted = DateTime.Now.AddDays(index).ToString("d"),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                var forecast = Models.WeatherForecast.NewRandom();
+                forecast.DateFormatted = DateTime.Now.AddDays(index).ToString("d");
+                
+                return forecast;
             });
-        }
-
-        public class WeatherForecast
-        {
-            public string DateFormatted { get; set; }
-            public int TemperatureC { get; set; }
-            public string Summary { get; set; }
-
-            public int TemperatureF
-            {
-                get
-                {
-                    return 32 + (int)(TemperatureC / 0.5556);
-                }
-            }
         }
     }
 }
---
+++ Models/WeatherForecast.cs
@@ -0,0 +1,34 @@
+namespace netdbdemo.Models
+{
+
+    public class WeatherForecast
+    {
+
+        private static string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public string DateFormatted { get; set; }
+        public int TemperatureC { get; set; }
+        public string Summary { get; set; }
+
+        public static WeatherForecast NewRandom() {
+
+            var rng = new Random();
+
+            return new WeatherForecast {
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            };
+        }
+
+        public int TemperatureF
+        {
+            get
+            {
+                return 32 + (int)(TemperatureC / 0.5556);
+            }
+        }
+    }
+}
